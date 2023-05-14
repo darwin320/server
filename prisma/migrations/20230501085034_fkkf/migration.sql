067b1feb239b367f64d728ad07f85787d5c569fe
darwin320 <darwinleonardovargas@gmail.com>
2023-05-14 00:47:17 -0500

    F

--- prisma/migrations/20230501085034_fkkf/migration.sql
+++ prisma/migrations/20230501085034_fkkf/migration.sql
@@ -59,18 +59,18 @@ CREATE TABLE `Config` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `Service` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `nameService` VARCHAR(50) NOT NULL,
-    `typeService` ENUM('type1', 'type2', 'type3') NOT NULL,
-    `nameSupplier` VARCHAR(50) NOT NULL,
-    `company` VARCHAR(50) NOT NULL,
-    `phoneNumber` VARCHAR(200) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
-    `price` DOUBLE NULL,
+    CREATE TABLE `Service` (
+        `id` INTEGER NOT NULL AUTO_INCREMENT,
+        `nameService` VARCHAR(50) NOT NULL,
+        `typeService` ENUM('type1', 'type2', 'type3') NOT NULL,
+        `nameSupplier` VARCHAR(50) NOT NULL,
+        `company` VARCHAR(50) NOT NULL,
+        `phoneNumber` VARCHAR(200) NOT NULL,
+        `description` VARCHAR(191) NOT NULL,
+        `price` DOUBLE NULL,
 
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+        PRIMARY KEY (`id`)
+    ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `Reservacion` (
